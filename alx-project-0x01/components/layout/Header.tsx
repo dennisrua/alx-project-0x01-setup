cf8b6d32f512071bba143133523ef9c52b3192cb
Dennis Rua <ruadennis20@gmail.com>
2025-09-19 16:15:07 +0300

    feat: add navigation between home, posts, and users pages

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -2,12 +2,22 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-900 text-white p-4">
-      <nav className="flex space-x-6">
-        <Link href="/">Home</Link>
-        <Link href="/">Posts</Link>
-        <Link href="/">Users</Link>
-      </nav>
+    <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <h3 className="font-bold text-2xl">
+          <Link href="/">Daily Contents</Link>
+        </h3>
+        <nav>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/posts">Posts</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
